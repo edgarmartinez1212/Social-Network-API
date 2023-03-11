7c21a7f5768b5bd71664baee3bb301de05004439
Edgar Martinez <edgarmartinez@Edgars-MBP.attlocal.net>
2023-03-11 15:18:54 -0600

    created controllers and thought.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThought).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+module.exports = router;
